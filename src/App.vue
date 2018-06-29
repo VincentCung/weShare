ae268ac6835302b7baa6a23a3694034c59d2a8fd
cai donghong <542677965@qq.com>
2018-06-29 15:14:48 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
+    <h1>{{message}}</h1>
+    <template v-if ="seen">
+      <h1>god</h1>
+      </template> 
   </div>
 </template>
 
 <script>
 export default {
-  name: "App"
-};
+  name: "App",
+  data() {
+    return {
+      message: "Hello World!",
+      seen: "seen"
+      };
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
+
