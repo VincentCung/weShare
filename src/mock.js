98bb4247ba354aaadf11ee57c534e51e2000577f
VincentCung <326216746@qq.com>
2018-07-07 15:05:20 +0800

    finish searchResult

--- src/mock.js
+++ src/mock.js
@@ -1,6 +1,23 @@
 const Mock = require('mockjs')
 const Random = Mock.Random
 //使用mockjs模拟数据
+function parseUrl(url) {
+    var queryObj = {};
+    var reg = /[?&]([^=&#]+)=([^&#]*)/g;
+    var querys = url.match(reg);
+    if (querys) {
+        for (var i in querys) {
+            var query = querys[i].split('=');
+            var key = query[0].substr(1),
+                value = query[1];
+            queryObj[key] ? queryObj[key] = [].concat(queryObj[key], value) : queryObj[key] = value;
+        }
+    }
+    return queryObj;
+}
+
+
+
 Mock.mock('/user/login', 'post', (req, res) => {
     info = JSON.parse(req.body)
     let result = {}
@@ -17,7 +34,7 @@ Mock.mock('/user/login', 'post', (req, res) => {
             is_banned: 0
         }
         result.msg.token = '1234'
-    }else {
+    } else {
         result.msg.success = 0
     }
     return result
@@ -85,21 +102,21 @@ Mock.mock(/\/message\/user/, 'get', {
             "name|1-5": "★",
             "photo": "@dataImage('200x100','头像')"
         },
-        counter:{
-            "count_follow|0-999":1,
-            "count_fans|0-999":1,
-            "count_weibo|0-999":1
+        counter: {
+            "count_follow|0-999": 1,
+            "count_fans|0-999": 1,
+            "count_weibo|0-999": 1
         },
-        "follow|0-5":[
+        "follow|0-5": [
             {
-                "id|+1":1,
+                "id|+1": 1,
                 "name|1-5": "★",
                 "photo": "@dataImage('100x100','头像')"
             }
         ],
-        "interests|0-5":[
+        "interests|0-5": [
             {
-                "id|+1":1,
+                "id|+1": 1,
                 "name|1-5": "★",
                 "photo": "@dataImage('100x100','头像')"
             }
@@ -107,17 +124,72 @@ Mock.mock(/\/message\/user/, 'get', {
     }
 })
 
-Mock.mock('/message/modify_pass','post',(req,res)=>{
-    let result={
+Mock.mock('/message/modify_pass', 'post', (req, res) => {
+    let result = {
         satusCode: 200,
         msg: {
             success: 0,
         }
     }
     info = JSON.parse(req.body)
-    if(info.old_password=='123'){
+    if (info.old_password == '123') {
         result.msg.success = 1
     }
     return result
+})
+
+Mock.mock(/\/weibo\/search/, 'get', (req, res) => {
+    let query = parseUrl(req.url)
+    if (query.sort == 0) {
+        return Mock.mock({
+            statusCode: 200,
+            msg: {
+                success: 1,
+                "weibos|1-20": [
+                    {
+                        "id|+1": 1,
+                        content: {
+                            "create_time": '@date("yyyy年MM月dd日") @time("HH:mm")',
+                            "context": '@string(7, 300)',
+                            "read_count|2-4": 1,
+                            "comment_count|2-4": 1,
+                            "transmit_count|2-4": 1,
+                            "thumb_count|2-4": 1,
+                            "photos|0-9": [
+                                {
+                                    "id|+1": 1,
+                                    "source": "@dataImage('200x100','照片')"
+                                }
+                            ]
+                        },
+                        user: {
+                            "photo": "@dataImage('100x100','头像')",
+                            "name": '@string(3, 10)',
+                        }
+                    }
+                ]
+            }
+        })
+
+    } else {
+        return Mock.mock({
+            statusCode: 200,
+            msg: {
+                success: 1,
+                "users|1-20": [
+                    {
+                        "id|+1": 1,
+                        "name": '@string(3, 10)',
+                        "photo": "@dataImage('100x100','头像')",
+                        "gender|0-2":1
+                        
+                    }
+                ]
+            }
+        })
+    }
+
+
+
 })
 
--- src/pages/SearchResult.vue
+++ src/pages/SearchResult.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="main">
     <div class="tab-box">
-      <el-tabs style="height: 200px;">
-        <el-tab-pane label="找微博">
-          <weibo v-for="weibo in weibos" :content='weibo.content' :key="weibo.id" style="background-color:#e1e1e1" :name="weibo.user.name" :avatar-url="weibo.user.photo" :id="weibo.id"/>
+      <el-tabs>
+        <el-tab-pane label="找微博" name="0">
+          <weibo v-for="weibo in weibos" :content='weibo.content' :key="weibo.id" style="background-color:#e1e1e1" :name="weibo.user.name" :avatar-url="weibo.user.photo" :id="weibo.id" />
           <div class='nothing-tip' v-if='!weibos.length'>
             <h3>找不到微博呢..</h3>
           </div>
         </el-tab-pane>
-        <el-tab-pane label="找人">
+        <el-tab-pane label="找人" name="1">
           <router-link v-for="item in users" :key="item.id" :to="'/blogs?userId='+item.id">
             <div class="user-card-box">
               <div class="box-avatar">
@@ -33,66 +33,8 @@ export default {
   },
   data() {
     return {
-      users: [
-        {
-          id: 1,
-          photo: "https://img.xiaopiu.com/userImages/img141644e3b5688.jpg",
-          name: "123",
-          gender: 1
-        },
-        {
-          id: 2,
-          photo: "https://img.xiaopiu.com/userImages/img141644e3b5688.jpg",
-          name: "123",
-          gender: 1
-        }
-      ],
-      weibos: [
-        {
-          id: 1,
-          content: {
-            create_time: '@date("yyyy年MM月dd日") @time("HH:mm")',
-            context: "@string(7, 300)",
-            read_count: 1,
-            comment_count: 1,
-            transmit_count: 1,
-            thumb_count: 1,
-            photos: [
-              {
-                id: 1,
-                source:
-                  "https://img.xiaopiu.com/userImages/img141644e3b5688.jpg"
-              }
-            ]
-          },
-          user: {
-            photo: "https://img.xiaopiu.com/userImages/img141644e3b5688.jpg",
-            name: "用户名"
-          }
-        },
-        {
-          id: 2,
-          content: {
-            create_time: '@date("yyyy年MM月dd日") @time("HH:mm")',
-            context: "@string(7, 300)",
-            read_count: 1,
-            comment_count: 1,
-            transmit_count: 1,
-            thumb_count: 1,
-            photos: [
-              {
-                id: 3,
-                source:
-                  "https://img.xiaopiu.com/userImages/img141644e3b5688.jpg"
-              }
-            ]
-          },
-          user: {
-            photo: "https://img.xiaopiu.com/userImages/img141644e3b5688.jpg",
-            name: "用户名"
-          }
-        }
-      ]
+      users: [],
+      weibos: []
     };
   },
   methods: {
@@ -115,7 +57,21 @@ export default {
         }
       })
       .then(response => {
-        // this.weibos = response.data.msg.weibos;
+        this.weibos = response.data.msg.weibos;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    this.$_http
+      .get("/weibo/search", {
+        params: {
+          sort: 1,
+          keyword: this.$route.query.keyword
+        }
+      })
+      .then(response => {
+        this.users = response.data.msg.users;
       })
       .catch(error => {
         console.log(error);
